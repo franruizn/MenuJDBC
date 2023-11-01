39beb7542cc836278412568df9f49cb981a56b68
Francisco Ruiz Nieto <97099217+franruizn@users.noreply.github.com>
2023-11-01 17:26:09 +0100

    Configuracion Clase Doctor

--- src/Modelo/Doctor.java
+++ src/Modelo/Doctor.java
@@ -9,5 +9,48 @@ package Modelo;
  * @author ivanf
  */
 public class Doctor {
+    private int iddoctor, fk_idespecialidad;
+    private String nombre, fk_idusuario;
+
+    public Doctor(){}
+    
+    public Doctor(int iddoctor, int fk_idespecialidad, String nombre, String fk_idusuario) {
+        this.iddoctor = iddoctor;
+        this.fk_idespecialidad = fk_idespecialidad;
+        this.nombre = nombre;
+        this.fk_idusuario = fk_idusuario;
+    }
+
+    public int getIddoctor() {
+        return iddoctor;
+    }
+
+    public void setIddoctor(int iddoctor) {
+        this.iddoctor = iddoctor;
+    }
+
+    public int getFk_idespecialidad() {
+        return fk_idespecialidad;
+    }
+
+    public void setFk_idespecialidad(int fk_idespecialidad) {
+        this.fk_idespecialidad = fk_idespecialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFk_idusuario() {
+        return fk_idusuario;
+    }
+
+    public void setFk_idusuario(String fk_idusuario) {
+        this.fk_idusuario = fk_idusuario;
+    }
     
 }
